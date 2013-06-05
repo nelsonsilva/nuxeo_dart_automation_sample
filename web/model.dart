8dfef25d3da4f211ec5d868e15606d387a40d12a
Nelson Silva <nelson.silva@inevo.pt>
2013-06-05 16:58:16 +0100

    Added widget and operation components and started using bootstrap

--- web/model.dart
+++ web/model.dart
@@ -1,6 +1,33 @@
 library model;
 
+import 'package:web_ui/observe.dart';
+import 'package:web_ui/observe/html.dart';
 import 'package:nuxeo/automation.dart' as nuxeo;
 import 'package:nuxeo/http/client.dart' as http;
 
-final NX = new nuxeo.Automation(new http.Client());
+nuxeo.Automation NX = null;
+
+var nuxeoUrl = "http://localhost:8080/nuxeo/site/automation";
+
+@observable
+bool isConnected = false;
+
+@observable
+Map<String, List> categories = toObservable({});
+
+connect() {
+  var client = new http.Client(nuxeoUrl);
+  NX = new nuxeo.Automation(client);
+
+  NX.registry.then((nuxeo.OperationRegistry registry) {
+    isConnected = true;
+    registry.ops.forEach((name, nuxeo.Operation op) {
+      if (!categories.containsKey(op.category)) {
+        categories[op.category] = toObservable([]);
+      }
+      categories[op.category].add(op);
+    });
+  });
+}
+
+
